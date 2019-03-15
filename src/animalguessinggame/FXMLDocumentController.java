f79cb997f293f379f906917bf638c8c24b714635
TreliaGT <v244682@tafe.wa.edu.au>
2019-03-15 16:23:07 +0800

    added GUI Layout

--- src/animalguessinggame/FXMLDocumentController.java
+++ src/animalguessinggame/FXMLDocumentController.java
@@ -10,7 +10,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 /**
  *
@@ -18,10 +22,36 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private Pane PaddQuestion;
+    @FXML
+    private TextField txtQuestion;
+    @FXML
+    private CheckBox CheckYes;
+    @FXML
+    private CheckBox CheckNo;
+    @FXML
+    private Label LaddQuestion;
+    @FXML
+    private Button btnAddQuestion;
+    @FXML
+    private Pane PgiveUp;
+    @FXML
+    private Button btnEnterNewAnimal;
+    @FXML
+    private TextField txtnewAnimal;
+    @FXML
+    private Pane Pmain;
+    @FXML
+    private Label LQuestion;
+    @FXML
+    private CheckBox CheckQYes;
+    @FXML
+    private CheckBox CheckQNo;
+    @FXML
+    private Button btnEnterInput;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
