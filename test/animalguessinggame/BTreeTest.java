c8c0a41933dbe9756abb1144ebc7cb17f445d94f
GT <v244682@tafe.wa.edu.au>
2019-05-19 09:44:30 +0800

    added unit tests

--- test/animalguessinggame/BTreeTest.java
+++ test/animalguessinggame/BTreeTest.java
@@ -42,6 +42,33 @@ public class BTreeTest {
         assertEquals(expResult, result);
     }
     
- 
+    
+     Node root = new Node("Does this animal have fur?");
+     Node node2 = new Node("It is a wolf?");
+   /**
+     * Test of getRoot method, of class BTree.
+     */
+    @Test
+    public void testGetRoot2() {
+        BTree instance = new BTree();
+        instance.setRoot(root);
+        String expResult = "Does this animal have fur?";
+        String result = instance.getRoot().getData();
+        assertEquals(expResult, result);
+    }
+
+
+    /**
+     * Test of getCurrentNode method, of class BTree.
+     */
+    @Test
+    public void testGetCurrentNode2() {
+        root.setYes(node2);
+        BTree instance = new BTree();
+        instance.setCurrentNode(root.getYes());
+        String expResult = "It is a wolf?";
+        String result = instance.getCurrentNode().getData();
+        assertEquals(expResult, result);
+    }
     
 }
